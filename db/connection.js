2757c77f2fa6efd0cf20e6118bfc37c60845d2b2
ejsadiarin <ekwtech.container720@passinbox.com>
2024-09-28 22:15:55 +0800

    feat(db): add mongodb connection with envs

--- db/connection.js
+++ db/connection.js
@@ -1,3 +1,13 @@
-/* TODO: connect to mongodb
- * - SUGGESTION: use docker for mongodb hehehe
- * */
+import mongoose from "mongoose";
+
+const dbConnection = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("Successfully connected to MongoDB.");
+    } catch (e) {
+        console.error(e);
+        throw e;
+    }
+};
+
+export default dbConnection;
--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 import express from "express";
-import { config } from "dotenv";
-import cors from "cors"
+import dotenv from "dotenv";
+import cors from "cors";
+import dbConnection from "./db/connection.js";
 
-config();
+const env = process.env.NODE_ENV || "development";
+if (env === "production") {
+    dotenv.config({ path: ".env.production" });
+} else {
+    dotenv.config({ path: ".env.development" });
+}
 
 const app = express();
-const port = 3000; // const port = process.env.API_PORT || 3000;
+const port = process.env.API_PORT || 3000;
+
+// call db connection
+dbConnection();
 
 // #region middleware
 
@@ -17,12 +26,11 @@ app.use(express.json());
 // #endregion middleware
 
 // test: curl http://localhost:3000/
-app.use('/', (req, res) => {
-    console.log("HEALTHY")
-    res.status(200).send('HEALTHY')
+app.use("/", (req, res) => {
+    console.log("HEALTHY");
+    res.status(200).send("HEALTHY");
 });
 
 app.listen(port, () => {
     console.log(`API listening on port ${port}`);
 });
-
