61a29dab6b4e997759bd2b3e8fc38478028e35de
ejsadiarin <ekwtech.container720@passinbox.com>
2024-09-30 10:49:32 +0800

    refactor: separation

--- controllers/routes.js
+++ controllers/routes.js
@@ -2,28 +2,28 @@ import express from 'express'; // const express = require('express');
 import {
     createOrganization,
     deleteOrganization,
-    getOrganization,
+    getOrganizationById,
     updateOrganization,
 } from '../services/organizationService.js';
-import { createUser, deleteUser, getUser, updateUser } from '../services/userService.js';
-import { createPost, deletePost, getPost, updatePost } from '../services/postService.js';
+import { createUser, deleteUser, getUserById, updateUser } from '../services/userService.js';
+import { createPost, deletePost, getPostById, updatePost } from '../services/postService.js';
 
 const router = express.Router();
 
 /* ***** ORGANIZATIONS ***** */
 router.post('/orgs', createOrganization);
-router.get('/org/:id', getOrganization);
-router.put('/org/:id', updateOrganization);
-router.delete('/org/:id', deleteOrganization);
+router.get('/orgs/:id', getOrganizationById);
+router.put('/orgs/:id', updateOrganization);
+router.delete('/orgs/:id', deleteOrganization);
 
 /* ***** USERS ***** */
 router.post('/users', createUser);
-router.get('/user/:id', getUser);
-router.put('/user/:id', updateUser);
-router.delete('/user/:id', deleteUser);
+router.get('/users/:id', getUserById);
+router.put('/users/:id', updateUser);
+router.delete('/users/:id', deleteUser);
 
 /* ***** POSTS ***** */
 router.post('/posts', createPost);
-router.get('/post/:id', getPost);
-router.put('/post/:id', updatePost);
-router.delete('/post/:id', deletePost);
+router.get('/posts/:id', getPostById);
+router.put('/posts/:id', updatePost);
+router.delete('/posts/:id', deletePost);
