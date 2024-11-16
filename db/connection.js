7f439c2fdda112714576f9d0332899f8a169f7be
ejsadiarin <kyube.ejs@proton.me>
2024-11-16 14:24:34 +0800

    chore: update env vars for connection
    need prod GOOGLE_CLIENT_ID and GOOGLE_CLIENT_SECRET to work

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const dbConnection = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URL);
         console.log("Successfully connected to MongoDB.");
     } catch (e) {
         console.error(e);
