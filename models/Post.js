9cdc1c5de879010f211cf4b4d51714a4a40a094b
roedzn <rohann_dizon@dlsu.edu.ph>
2025-01-29 01:50:21 +0800

    refactor: needed changes on post model

--- models/Post.js
+++ models/Post.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 
 const PostSchema = new mongoose.Schema({
     title: { type: String, required: true },
-    content: { type: String, required: true },
-    category: { type: String, required: true, },
-    featuredImg: { data: Buffer, imgType: String, },
+    content: { type: Object, required: true },
+    category: { type: String, required: true },
+    featuredImg: { data: Buffer, imgType: String },
     createdAt: { type: Date, default: Date.now },
-    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    orgIds: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Organization', required: true, }],
+    author: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }],
+    orgIds: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Organization', required: true }],
     comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
 });
 
